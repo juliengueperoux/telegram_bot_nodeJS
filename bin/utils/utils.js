60421b57e76fc7f6ff1b34960776502f9f62b11a
AirJulien <julien.gueperoux@gamil.com>
2020-04-22 21:26:25 +0200

    userLostTimer correction

--- bin/utils/utils.js
+++ bin/utils/utils.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const querystring = require('querystring');
 
 // user lost timeout value 30 seconds
-exports.USER_LOST_TIMEOUT = 5000; 
+exports.USER_LOST_TIMEOUT = 30000; 
 
 // With Api base Url With video Name
 exports.WITH_API_VIDEO_URL = `${process.env.WITH_API_BASE}video/${querystring.escape(process.env.WITH_VIDEO_NAME)}`; 
